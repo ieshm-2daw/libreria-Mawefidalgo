abc99d0f2bd49efe97afc329ef50b18da6d8e7ad
mawefidalgo <mawefidalgo7@gmail.com>
2023-11-30 12:25:47 +0100

    Editar y Borrar implementado

--- AppLibreria/urls.py
+++ AppLibreria/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import Listado, Detalles, Crear
+from .views import Listado, Detalles, Crear, Borrar, Editar
 
 urlpatterns = [
     path('',Listado.as_view(), name='Listado'),
     path('detalles/<int:pk>/', Detalles.as_view(), name='detalles'),
     path('nuevo', Crear.as_view(),name='crear'),
+    path('borrar/<int:pk>/',Borrar.as_view(),name='borrar'),
+    path('editar/<int:pk>/', Editar.as_view(), name='editar'),
 ]
--- AppLibreria/views.py
+++ AppLibreria/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Libro
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from typing import Any
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 # Create your views here.
 class Listado(ListView):
 
@@ -11,13 +12,13 @@ class Listado(ListView):
     queryset = Libro.objects.filter(disponibilidad="disponible")
 
 
-def get_context_data(self, **kwargs: Any) -> dict(str, Any):
-    context = super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs: Any) -> dict:
+            context = super().get_context_data(**kwargs)
 
-    context['libro_disponibles'] = Libro.objects.filter(disponibilidad="disponible")
-    context['libros_prestados'] = Libro.objects.filter(disponibilidad="prestado")
+            context['libro_disponibles'] = Libro.objects.filter(disponibilidad="disponible")
+            context['libros_prestados'] = Libro.objects.filter(disponibilidad="prestado")
 
-    return context
+            return context
 
 class Detalles(DetailView):
 
@@ -30,3 +31,15 @@ class Crear(CreateView):
     fields = ['titulo','autor','editorial','rating','fechapubli','genero','ISBN','resumen','disponibilidad','portada']
     template_name = 'AppLibreria/crear.html'
     success_url = reverse_lazy('Listado')
+
+
+class Borrar(DeleteView):
+     model = Libro
+     template_name = "AppLibreria/borrar.html"
+     success_url = reverse_lazy('Listado')
+
+class Editar(UpdateView):
+     model = Libro
+     fields = ['titulo','autor','editorial','rating','fechapubli','genero','ISBN','resumen','disponibilidad','portada']
+     template_name = 'AppLibreria/editar.html'
+     success_url = reverse_lazy('Listado')
